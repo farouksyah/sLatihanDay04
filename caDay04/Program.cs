b6d6dc461b1a16c57968c18860c929b788be7790
farouksyah <farouksyah@gmail.com>
2019-09-29 20:14:30 -0700

    isi console application

---
+++ caDay04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay04
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // perintah unuk cetak hasil
+            Console.WriteLine("Selamat Datang batch 215");
+
+            // trick nunggu input
+            // agat tidak otomatis close
+            Console.ReadLine();
+        }
+    }
+}
